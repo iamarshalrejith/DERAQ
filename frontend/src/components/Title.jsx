4a211ad23834a6a09ec7887467106efec3994018
Arshal Rejith <iamarshalrejith@gmail.com>
2025-12-28 10:20:24 +0530

    Added Latest Collection

---
+++ frontend/src/components/Title.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Title = ({text1,text2}) => {
+  return (
+    <div className='inline-flex gap-2 items-center mb-3'>
+        <p className='text-blue-950'>
+            {text1} <span className='text-blue-950 font-medium'>{text2}</span>
+        </p>
+        <p className='w-8 sm:w-12 h-[1px] sm:h-[2px] bg-blue-950'></p>
+    </div>
+  )
+}
+
+export default Title;
